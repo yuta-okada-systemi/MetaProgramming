c0bcb69e07a8fe5c512b154ee81b8796db239f64
yuta.okada <yuta.okada@systemi.co.jp>
2025-07-17 21:26:31 +0900

    ExpressionTreeを使ったプロパティコピー処理を追加

---
+++ MetaProgramming.Tests/Samples/ExpressionTreeDxoTest.cs
@@ -0,0 +1,46 @@
+using System;
+using MetaProgramming.Samples;
+
+namespace Samples.Tests
+{
+    public class ExpressionTreeDxoTest
+    {
+        [Fact]
+        public void CreateObjectTest1()
+        {
+            var src = new StringModel()
+            {
+                StringValue = "abcdefg",
+                IntValue = "123456",
+                DoubleValue = "123.456",
+                DatetimeValue = "2025/07/18 18:15:00"
+            };
+            var dest = ExpressionTreeDxo.CreateObject<StringModel, TypeModel>(src);
+
+            Assert.NotNull(dest);
+            Assert.Equal("abcdefg", dest.StringValue);
+            Assert.Equal(123456, dest.IntValue);
+            Assert.Equal(123.456, dest.DoubleValue);
+            Assert.Equal(new DateTime(2025, 7, 18, 18, 15, 0), dest.DatetimeValue);
+        }
+
+        [Fact]
+        public void CreateObjectTest2()
+        {
+            var src = new StringModel()
+            {
+                StringValue = "abcdefg",
+                IntValue = "123456",
+                DoubleValue = "123.456",
+                DatetimeValue = "2025/07/18 18:15:00"
+            };
+            var dest = ExpressionTreeDxo.CreateObject<StringModel, NullableTypeModel>(src);
+
+            Assert.NotNull(dest);
+            Assert.Equal("abcdefg", dest.StringValue);
+            Assert.Equal(123456, dest.IntValue);
+            Assert.Equal(123.456, dest.DoubleValue);
+            Assert.Equal(new DateTime(2025, 7, 18, 18, 15, 0), dest.DatetimeValue);
+        }
+    }
+}
---
+++ MetaProgramming/Samples/ExpressionTreeDxo.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MetaProgramming.Samples;
+
+public static class ExpressionTreeDxo {
+    /// <summary>
+    /// generic cache
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    private static class Cache<T> {
+        public static readonly PropertyInfo[] properties;
+        public static readonly Func<T> CreateDelegate;
+
+        static Cache() {
+            properties = typeof(T).GetProperties(BindingFlags.Public |
+                BindingFlags.NonPublic |
+                BindingFlags.Instance).Where(x => x.GetIndexParameters().Length < 1).ToArray();
+
+            Func<T> newDelegate = null;
+            if (typeof(T).IsClass || typeof(T).IsValueType) {
+                if (typeof(T).GetConstructor(Type.EmptyTypes) != null) {
+                    newDelegate = CreateNewExpression().Compile();
+                }
+            }
+            CreateDelegate = newDelegate;
+        }
+
+        public static Expression<Func<T>> CreateNewExpression() {
+            //  T instance;
+            var instance = Expression.Variable(typeof(T), "instance");
+
+            var returnTarget = Expression.Label(typeof(T));
+            var returnLabel = Expression.Label(returnTarget, instance);
+
+            var newBlock = Expression.Block(typeof(T), [instance], [
+                //  instance = new T();
+                Expression.Assign(instance, Expression.New(typeof(T))),
+                // return instance;
+                returnLabel
+            ]);
+
+            return Expression.Lambda<Func<T>>(newBlock);
+        }
+    }
+
+    private static class DxoCache<SRC, DST> {
+        public static Action<SRC, DST> copyMethod;
+
+        static DxoCache() {
+            copyMethod = CreateCopyPropertiesAction();
+        }
+
+        private static Action<SRC, DST> CreateCopyPropertiesAction() {
+            var props1 = Cache<SRC>.properties;
+            var props2 = Cache<DST>.properties;
+            // プロパティ名が一致する対象同士でコピーする
+            var items = props1.Join(props2, p1 => p1.Name, p2 => p2.Name, (src, dest) => {
+                return new { src, dest };
+            });
+
+            var srcExp = Expression.Parameter(typeof(SRC), "src");
+            var dstExp = Expression.Parameter(typeof(DST), "dest");
+            var expressions = items.Where(item => item.src.CanRead && item.dest.CanWrite)
+                                    .Select(item => {
+                                        // コピープロパティからの値取得
+                                        var valueExpression = Expression.Call(
+                                            srcExp, item.src.GetGetMethod(true)
+                                        );
+
+                                        // 型変換
+                                        var method = GetConvertValueMethod(item.src.PropertyType, item.dest.PropertyType);
+                                        var converterdExp = Expression.Call(method, valueExpression);
+
+                                        // コピー先への値設定
+                                        return Expression.Call(dstExp, item.dest.GetSetMethod(true), converterdExp);
+                                    });
+            return Expression.Lambda<Action<SRC, DST>>(
+                Expression.Block(expressions.ToList()),
+                [srcExp, dstExp]
+            ).Compile();
+        }
+    }
+
+    private static readonly ConcurrentDictionary<Tuple<Type, Type>, MethodInfo> ConverterCache = [];
+
+    public static DST ConvertValue<SRC, DST>(SRC value) {
+        return MyTypeConverter.ConvertValue<SRC, DST>(value);
+    }
+
+    /// <summary>
+    ///  srcのプロパティ値をコピーしたTのインスタンスを生成（プロパティ名が一致する対象同士で値コピー）
+    /// </summary>
+    /// <typeparam name="SRC"></typeparam>
+    /// <typeparam name="DST"></typeparam>
+    /// <param name="src"></param>
+    /// <returns></returns>
+    public static DST CreateObject<SRC, DST>(SRC src) where DST : new() {
+        if (src is null) return default;
+
+        var dest = Cache<DST>.CreateDelegate();
+        return CopyProperties(src, dest);
+    }
+
+    /// <summary>
+    ///  src→destへのプロパティ値コピー（プロパティ名が一致する対象同士で値コピー）
+    /// </summary>
+    /// <typeparam name="SRC"></typeparam>
+    /// <typeparam name="DST"></typeparam>
+    /// <param name="src"></param>
+    /// <param name="dest"></param>
+    /// <returns></returns>
+    public static DST CopyProperties<SRC, DST>(SRC src, DST dest) {
+        if (src is not null && dest is not null) {
+            DxoCache<SRC, DST>.copyMethod.Invoke(src, dest);
+        }
+        return dest;
+    }
+
+    private static MethodInfo GetConvertValueMethod(Type srcType, Type destType) {
+        var key = Tuple.Create(srcType, destType);
+        if (!ConverterCache.TryGetValue(key, out var method)) {
+            var nonGenericMethod = typeof(MyTypeConverter).GetMethod(nameof(MyTypeConverter.ConvertValue), BindingFlags.Static | BindingFlags.Public);
+            method = nonGenericMethod.MakeGenericMethod([srcType, destType]);
+            ConverterCache.TryAdd(key, method);
+        }
+        return method;
+    }
+}
